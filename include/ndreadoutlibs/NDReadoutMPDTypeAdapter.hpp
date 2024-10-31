3189af42d98fb0c749b167b3b60368bdaddc4a75
Henry Wallace <henry.wallace@rhul.ac.uk>
2024-10-31 11:07:27 +0000

    updates for compatibility

--- include/ndreadoutlibs/NDReadoutMPDTypeAdapter.hpp
+++ include/ndreadoutlibs/NDReadoutMPDTypeAdapter.hpp
@@ -17,55 +17,55 @@
 #include <memory>  // shared_ptr
 
 namespace dunedaq {
-  namespace ndreadoutlibs {
-    namespace types {
-      /**
-       * @brief MPD frame
-       * */
-      struct NDReadoutMPDTypeAdapter {
+namespace ndreadoutlibs {
+namespace types {
+/**
+ * @brief MPD frame
+ * */
+struct NDReadoutMPDTypeAdapter {
 	using FrameType = NDReadoutMPDTypeAdapter;
 	std::vector<char> data ;
 
 	void load_message( const void * load_data, const unsigned int size ) {
-	  data.resize(size);
-	  memcpy(&data[0], load_data, size);
+		data.resize(size);
+		memcpy(&data[0], load_data, size);
 	}
 
 	bool operator<(const NDReadoutMPDTypeAdapter & other) const
 	{
-          uint64_t this_timestamp  = 0;  //NOLINT
-          uint64_t other_timestamp = 0;  //NOLINT
+		uint64_t this_timestamp  = 0;  //NOLINT
+		uint64_t other_timestamp = 0;  //NOLINT
 
-          // 27-Apr-2023, KAB: the size of the data member needs to be checked to see whether it
-          // actually contains any data, before trying to interpret the contents.
-          if (data.size() >= sizeof(dunedaq::nddetdataformats::MPDFrame)) {
-            auto thisptr = reinterpret_cast<const dunedaq::nddetdataformats::MPDFrame*>(&data[0]);  // NOLINT
-            this_timestamp = thisptr->get_timestamp();
-          }
-          if (other.data.size() >= sizeof(dunedaq::nddetdataformats::MPDFrame)) {
-            auto otherptr = reinterpret_cast<const dunedaq::nddetdataformats::MPDFrame*>(&other.data[0]);  // NOLINT
-            other_timestamp = otherptr->get_timestamp();
-          }
+		// 27-Apr-2023, KAB: the size of the data member needs to be checked to see whether it
+		// actually contains any data, before trying to interpret the contents.
+		if (data.size() >= sizeof(dunedaq::nddetdataformats::MPDFrame)) {
+			auto thisptr = reinterpret_cast<const dunedaq::nddetdataformats::MPDFrame*>(&data[0]);  // NOLINT
+			this_timestamp = thisptr->get_timestamp();
+		}
+		if (other.data.size() >= sizeof(dunedaq::nddetdataformats::MPDFrame)) {
+		auto otherptr = reinterpret_cast<const dunedaq::nddetdataformats::MPDFrame*>(&other.data[0]);  // NOLINT
+		other_timestamp = otherptr->get_timestamp();
+		}
 
-	  return (this_timestamp < other_timestamp) ? true : false;
+	return (this_timestamp < other_timestamp) ? true : false;
 	}
-	
+
 	uint64_t get_timestamp() const // NOLINT(build/unsigned)
 	{
-	  auto thisptr = reinterpret_cast<const dunedaq::nddetdataformats::MPDFrame*>(&data[0]);        // NOLINT
-	  return thisptr->get_timestamp();
+	auto thisptr = reinterpret_cast<const dunedaq::nddetdataformats::MPDFrame*>(&data[0]);        // NOLINT
+	return thisptr->get_timestamp();
 	}
 
 	uint64_t get_first_timestamp() const { return get_timestamp(); }
 
 	void set_first_timestamp(uint64_t /*ts*/) // NOLINT(build/unsigned)
 	{
-	  //
+	//
 	}
 
 	void set_timestamp(uint64_t /*ts*/) // NOLINT(build/unsigned)
 	{
-	  //
+	//
 	}
 
 	size_t get_payload_size() { return data.size(); }
@@ -80,9 +80,26 @@ namespace dunedaq {
 	static const constexpr daqdataformats::SourceID::Subsystem subsystem = daqdataformats::SourceID::Subsystem::kDetectorReadout;
 	static const constexpr daqdataformats::FragmentType fragment_type = daqdataformats::FragmentType::kMPD;
 
-      };
-    } // namespace types
-  } // namespace ndreadoutlibs
+	// Fake data properties
+	void fake_timestamps(uint64_t /*first_timestamp*/, uint64_t /*offset = 2048*/ ) // NOLINT(build/unsigned)
+	{
+	}
+
+	void fake_geoid(uint16_t /*crate_id*/, uint16_t /*slot_id*/, uint16_t /*stream_id*/) {
+	}
+
+	void fake_adc_pattern(int /*channel*/) {
+	}
+
+	void fake_frame_errors(std::vector<uint16_t>* /*fake_errors*/) // NOLINT
+	{
+	    // Set error bits in header
+	}
+
+
+};
+} // namespace types
+} // namespace ndreadoutlibs
 } // namespace dunedaq
 
 #endif // NDREADOUTLIBS_INCLUDE_NDREADOUTLIBS_NDREADOUTMPDTYPEADAPTER_HPP_
